e6dc28b39d096b8e13cd144f3fdb2338c7add071
James Scherer <jmscherer88@gmail.com>
2014-02-02 15:44:39 -0500

    Initial Commit

---
+++ SocialLubrication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocialLubrication
+//
+//  Created by James Scherer on 2/2/14.
+//  Copyright (c) 2014 Mazag Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
